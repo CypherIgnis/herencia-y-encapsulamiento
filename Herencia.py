efdbbff2f902124f46624691cc350b255693d843
CypherIgnis <116596423+CypherIgnis@users.noreply.github.com>
2022-12-05 17:02:07 -0300

    Update Herencia.py

--- Herencia.py
+++ Herencia.py
@@ -1,1 +1,24 @@
+class Worker:
+  
+    company_name='Sodimac'
+    company_domain='@sodimac.cl'
+
+
+    def __init__(self,id,first_name,last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name    
+        self.mail = self.first_name+'.'+self.last_name+self.company_domain
+
+    def print_info(self):
+        return print("Te damos la bienvenida a ",self.company_name," ",self.first_name,' ',self.last_name,'Y su correo laboral es',self.mail)
+
+class Developer(Worker):
+    def __init__(self,id,first_name,last_name,language):
+        super().__init__(id,first_name,last_name)        
+        self.language = language
+        
+    def print_language(self):
+        return print("El lenguaje de programación que se manejar es el:",self.language)
+
 
